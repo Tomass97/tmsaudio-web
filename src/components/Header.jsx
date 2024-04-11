e6ad461a032317b1df9048aa00f66d3e82feaf69
Tomas Preduna <tomasrlnd97@gmail.com>
2024-04-11 19:50:32 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,99 @@
+import { useMemo } from 'react'
+
+export default function Header({cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart}) {
+
+    // State Derivado
+    const isEmpty = useMemo( () => cart.length === 0, [cart])
+    const cartTotal = useMemo( () => cart.reduce( (total, item ) => total + (item.quantity * item.price), 0), [cart] )
+
+    return (
+        <header className="py-5 header">
+            <div className="container-xl">
+                <div className="row justify-content-center justify-content-md-between">
+                    <div className="col-8 col-md-3">
+                        <a href="index.html">
+                           <h2>TMS AUDIO</h2>
+                        </a>
+                    </div>
+                    <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+                        <div 
+                            className="carrito"
+                        >
+                            <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+
+                            <div id="carrito" className="bg-white p-3">
+                                {isEmpty ? (
+                                    <p className="text-center">El carrito esta vacio</p>
+                                ) : (
+                                <>
+                                    <table className="w-100 table">
+                                        <thead>
+                                            <tr>
+                                                <th>Imagen</th>
+                                                <th>Nombre</th>
+                                                <th>Precio</th>
+                                                <th>Cantidad</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {cart.map( headphone => (
+                                                <tr key={headphone.id}>
+                                                    <td>
+                                                        <img 
+                                                            className="img-fluid" 
+                                                            src={`/img/${headphone.image}`}
+                                                            alt="imagen casco" 
+                                                        />
+                                                    </td>
+                                                    <td>{headphone.name}</td>
+                                                    <td className="fw-bold">
+                                                        €{headphone.price}
+                                                    </td>
+                                                    <td className="flex align-items-start gap-4">
+                                                        <button
+                                                            type="button"
+                                                            className="btn btn-dark"
+                                                            onClick={() => decreaseQuantity(headphone.id)}
+                                                        >
+                                                            -
+                                                        </button>
+                                                            {headphone.quantity}
+                                                        <button
+                                                            type="button"
+                                                            className="btn btn-dark"
+                                                            onClick={() => increaseQuantity(headphone.id)}
+                                                        >
+                                                            +
+                                                        </button>
+                                                    </td>
+                                                    <td>
+                                                        <button
+                                                            className="btn btn-danger"
+                                                            type="button"
+                                                            onClick={() => removeFromCart(headphone.id)}
+                                                        >
+                                                            X
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+
+                                    <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
+                                </>
+                                )}
+
+                                <button 
+                                    className="btn btn-dark w-100 mt-3 p-2"
+                                    onClick={clearCart}
+                                >Vaciar Carrito</button>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
